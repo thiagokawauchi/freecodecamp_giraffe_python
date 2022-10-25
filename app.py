7623b5608d58ea649fa2998e320413cff0f60b28
thiagokawauchi <thiago.kawauchi@gmail.com>
2022-10-25 17:30:17 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# tutorial 1
+print('Hello World')
+
+# tutorial 2
+print('   /|')
+print('  / |')
+print(' /  |')
+print('/___|')
+
+# tutorial 3
+character_name = "Tom"
+character_age = "50"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
